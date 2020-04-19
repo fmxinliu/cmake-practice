4b21bfbf303740a41d19f68a261f83286efd4fc2
xin liu <xinliuchnsz@gmail.com>
2020-04-20 00:17:19 +0800

    添加动态库测试工程

---
+++ T15/build.bat
@@ -0,0 +1,16 @@
+@rem 构建编译目录
+@ rmdir build /Q /S
+@ mkdir build
+@ cd build/
+
+@rem 根据CMakeList.txt文件，生成平台相关的Makefile或工程文件
+@ cmake .. -G "Visual Studio 10 2010"
+
+@rem 编译
+@ "C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\IDE\devenv.exe" demo.sln /Rebuild "Release"
+
+@rem 运行
+@ cd bin/Release/
+@ hello.exe
+
+@ if not "%1"=="nopause" @ pause
