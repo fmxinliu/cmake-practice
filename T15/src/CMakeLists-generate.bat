a840f1aefae6caa5b4f01851f3c9693407acd568
xin liu <xinliuchnsz@gmail.com>
2020-04-24 21:16:10 +0800

    添加def模块

--- T15/src/CMakeLists-generate.bat
+++ T15/src/CMakeLists-generate.bat
@@ -1,33 +1,30 @@
-@setlocal enabledelayedexpansion
+@ setlocal enabledelayedexpansion
 
 @rem 文件路径
-@set file_path=CMakeLists.txt
-@set tmpl_path=CMakeLists.tmpl
-@set temp_path=CMakeLists.temp
+@ set file_path=CMakeLists.txt
+@ set tmpl_path=CMakeLists.tmpl
 
-@rem 动态库生成参数
-@set DLL_STYLE=DLL_STYLE_VALUE
-@set CALL_CONVENTIONS=CALL_CONVENTIONS_VALUE
-@set DLL_EXPORTS=DLL_EXPORTS_VALUE
-@set DLL_EXPORTS_DEF_FILE=DLL_EXPORTS_DEF_FILE_VALUE
+@rem 动态库导出参数
+@ set DLL_CALL_CONVENTIONS=DLL_CALL_CONVENTIONS_DEFINE
+@ set DLL_STYLE=DLL_STYLE_DEFINE
+@ set DLL_EXPORTS=DLL_EXPORTS_DEFINE
+@ set DLL_EXPORTS_DEF_FILE=DLL_EXPORTS_DEF_FILE_DEFINE
 
-@rem 临时文件
-@if exist %temp_path% @del %temp_path% /f /q
-
-@for /f "tokens=*" %%i in (%tmpl_path%) do @(
-    @set s=%%i
-    @set s=!s:%DLL_STYLE%=%1!
-    @set s=!s:%CALL_CONVENTIONS%=%2!
+@rem 生成 CMakeLists.txt
+@ if exist %file_path% @ del %file_path% /f /q
+@ for /f "tokens=*" %%i in (%tmpl_path%) do @(
+    @ set s=%%i
+    @ set s=!s:%DLL_STYLE%=%1!
+    @ set s=!s:%DLL_CALL_CONVENTIONS%=%2!
     @if %3==nopause @(
       @set s=!s:%DLL_EXPORTS%=DLL_EXPORTS!
     ) else (
       @set s=!s:%DLL_EXPORTS%=%3!
     )
     @set s=!s:%DLL_EXPORTS_DEF_FILE%=%4!
-    @echo !s!>>%temp_path%
+    @ echo !s! >> %file_path%
 )
 
-@move %temp_path% %file_path%
-
-@for %%a in (%*) do @if "%%a"=="nopause" @exit /b 0
-@pause
+@rem ---
+@ for %%a in (%*) do @ if "%%a"=="nopause" @ exit /b 0
+@ pause
