0acf37b51e4cc9a0c31dd33ad13d5735839e8def
xin liu <xinliuchnsz@gmail.com>
2020-04-27 21:47:27 +0800

    添加脚本：def方式导出动态库

---
+++ T15/build-scripts/defscripts/style-for-stdcall-c.bat
@@ -0,0 +1,1 @@
+@ cd ../ && call subscripts/libhello-export-style.bat __stdcall "extern \"C\"" DLL_EXPORTS_USE_DEF_FILE libhello.def
---
+++ T15/build-scripts/defscripts/style-for-stdcall.bat
@@ -0,0 +1,1 @@
+@ cd ../ && call subscripts/libhello-export-style.bat __stdcall "" DLL_EXPORTS_USE_DEF_FILE libhello.def
