5f5640653bc13f15655b9e1183b1b53ee597bdae
xin liu <xinliuchnsz@gmail.com>
2021-03-06 23:31:49 +0800

    修改自定义头文件的包含方式

--- T17/include/libhello.h
+++ T17/include/libhello.h
@@ -1,7 +1,7 @@
 #ifndef LIBHELLO_H
 #define LIBHELLO_H
 #include <stdio.h>
-#include <dllexport.h>
+#include "dllexport.h"
 
 #define SAYHELLO_FUNC_DECLARATION \
 extern "C" DLL_API void __stdcall sayHello()
