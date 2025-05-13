f53e738648374cb95f906ff66dd9257810972a27
liuxin <xinliuchnsz@gmail.com>
2025-05-13 09:14:24 +0800

    解决Qt5.0版本，icon.availableSizes()在QCoreApplication下crash，导致tst_images_qrc测试失败

--- qt/tests/resources/tst_images_qrc.cpp
+++ qt/tests/resources/tst_images_qrc.cpp
@@ -8,10 +8,31 @@ class tst_ImagesQrc : public QObject
     Q_OBJECT
 
 private Q_SLOTS:
+    void initTestCase();
+    void cleanupTestCase();
     void tst_load_icon_data();
     void tst_load_icon();
+
+private:
+    QString nonIconFilepath;
 };
 
+
+void tst_ImagesQrc::initTestCase()
+{
+    nonIconFilepath = QCoreApplication::applicationDirPath() + "/tst_images_qrc.txt";
+
+    QFile file(nonIconFilepath);
+    if (file.open(QIODevice::WriteOnly | QIODevice::Text)) {
+        file.write("This is a txt file");
+        file.close();
+    }
+}
+
+void tst_ImagesQrc::cleanupTestCase()
+{
+    QFile::remove(nonIconFilepath);
+}
 
 void tst_ImagesQrc::tst_load_icon_data()
 {
@@ -20,8 +41,9 @@ void tst_ImagesQrc::tst_load_icon_data()
 
     QTest::newRow("empty path") << "" << false;
     QTest::newRow("not exists path") << "path/to/image.png" << false;
-    QTest::newRow("load from file-path") << APP_ICON_PATH << true;
-    QTest::newRow("load from qrc-path") << ":images/app.ico" << true;
+    QTest::newRow("icon file from file-path") << APP_ICON_PATH << true;
+    QTest::newRow("icon file from qrc-path") << ":images/app.ico" << true;
+    QTest::newRow("non-icon file") << nonIconFilepath << false;
 }
 
 void tst_ImagesQrc::tst_load_icon()
@@ -35,7 +57,7 @@ void tst_ImagesQrc::tst_load_icon()
         qDebug() << path;
 
         QIcon icon(path);
-        if (!icon.isNull() /*&& !icon.availableSizes().isEmpty()*/)
+        if (!icon.isNull() && !icon.availableSizes().isEmpty())
         {
             iconLoaded = true;
         }
